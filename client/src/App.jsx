0c37072644d0c4315eb9038ac44835631b22a075
Iheb Nakib <nakibiheb409@gmail.com>
2023-11-26 21:57:51 +0100

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signout from "./pages/Signout";
+import About from "./pages/About";
+import Profil from "./pages/Profil";
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signout />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profil />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
